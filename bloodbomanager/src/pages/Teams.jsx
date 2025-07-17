4a44245e087a6a6114e54571fbf82eb34213ee14
Ibai <ibaiminiaturas@gmail.com>
2025-07-17 15:55:14 +0200

    fixed CORS error just by adding to accept json on the header of the edit team

--- bloodbomanager/src/pages/Teams.jsx
+++ bloodbomanager/src/pages/Teams.jsx
@@ -69,6 +69,7 @@ export default function Teams() {
                 method: 'PUT',
                 headers: {
                     'Content-Type': 'application/json',
+                    "Accept": "application/json",
                     Authorization: `Bearer ${token}`,
                 },
                 body: JSON.stringify({
